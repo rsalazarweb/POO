ee11c276b0f185019176b2182d62236899e2fdbe
Rodrigo <rsalazar.web@gmail.com>
2019-12-16 17:21:46 -0600

    Herencia en JS

---
+++ JS/UberBlack.js
@@ -0,0 +1,7 @@
+class UberBlack extends Car {
+    constructor(license, driver, typeAccepted, seatsMaterial) {
+        super(license, driver)
+        this.typeAccepted = typeAccepted;
+        this.seatsMaterial = seatsMaterial;
+    }
+}
--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,8 @@
 var car = new Car("MXA6585", new Account("Andres Herrera", "ide234"));
 car.passenger = 4;
 car.printDataCar();
+
+
+var uberX = new UberX("AWS1234", new Account("Juan Pérez", "ANDA1234"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
