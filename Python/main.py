7f6d72308d89b5e31fc9ed88aa5be50d4b4a1e88
Rodrigo <rsalazar.web@gmail.com>
2019-12-16 12:36:06 -0600

    Método constructor en python

--- Python/main.py
+++ Python/main.py
@@ -1,17 +1,9 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "LMF0456"
-    car.driver = "Juan Perez"
-    car.passenger = 4
-
+    
+    car = Car("AMS123", Account("Andres Herrera", "IDE12345"))
     print(vars(car))
-
-    car2 = Car()
-    car2.license = "SAR2345"
-    car2.driver = "Carlos Perez"
-    car2.passenger = 4
-
-    print(vars(car2))
+    print(vars(car.driver))
